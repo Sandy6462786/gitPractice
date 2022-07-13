a1cb6326a3cad3adcf9349bae5f4b53b75d7bb03
Sandeep Sharma <ssharma6462786@gmail.com>
2022-07-13 14:26:35 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,10 @@
+import java.nio.file.attribute.PosixFilePermission;
+
+public class Sum{
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        System.out.println(a+b);
+    }
+}
