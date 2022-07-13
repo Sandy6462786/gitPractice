baed647e8b0ab987caddb6caa54af9886c284ce6
Sandeep Sharma <ssharma6462786@gmail.com>
2022-07-13 15:04:37 +0530

    Changed Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -3,6 +3,6 @@ public class Multiply{
     public static void main(String[] args) {
         int a = 5;
         int b = 6;
-        System.out.println("The Multiply is: ", a*b);
+        System.out.println("The hello Multiply is: ", a*b);
     }
 }
